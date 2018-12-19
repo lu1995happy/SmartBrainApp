7a2e42064c749d83ae6a707405e69e6dafb4676d
Tianlin Lu <txl355@case.edu>
2018-12-19 15:32:55 -0800

    update server url

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users);})
+app.get('/', (req, res) => {res.send("It's working!");})
 
 app.post('/signin', signin.handleSignin(db, bcrypt));
 
